6f102a3b23400cc46599e7774b296d570ccbf78f
pragyanverse <pragyansingh791@gmail.com>
2026-05-24 21:49:39 +0530

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,8 @@
+a = float(input("Enter first: "))
+b = float(input("Enter second: "))
+avg = (a+b)/2
+print("Your Average is= ", avg)
+
+c = int(input("Enter first: "))
+d = int(input("Enter Second: "))
+print(c >= d)
